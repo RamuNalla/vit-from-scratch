36d2416e283d6efff14fb962a15f0b07af9233c8
RamuNalla <nallaramu4321@gmail.com>
2026-08-30 18:40:10 +0530

    vision transformer class

--- src/models/vit.py
+++ src/models/vit.py
@@ -2,3 +2,21 @@ import torch
 import torch.nn as nn
 from src.models.embeddings import ViTEmbeddings
 from src.models.block import TransformerEncoderBlock
+
+class VisionTransformer(nn.Module):
+    """
+    Complete Vision Transformer (ViT) Architecture from scratch.
+    """
+    def __init__(
+        self,
+        in_channels: int = 3,
+        image_size: int = 32,
+        patch_size: int = 4,
+        emb_dim: int = 64,
+        depth: int = 6,              # Number of transformer blocks
+        num_heads: int = 8,
+        expansion_factor: int = 4,
+        num_classes: int = 100,      # CIFAR-100 default
+        dropout: float = 0.1
+    ):
+        super().__init__()
